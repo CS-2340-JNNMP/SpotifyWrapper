29fa1096c445c300eb11ddc0eaf7fa790a6b8696
poojasaji1 <psaji3@gatech.edu>
2024-10-23 14:38:54 -0400

    added lgin and register

--- core/urls.py
+++ core/urls.py
@@ -8,9 +8,12 @@ urlpatterns = [
     # path('register_user/', views.register_user, name='register_user'),
     # path('profile/', views.profile, name='profile'),
     # path('logout/', views.logout_user, name='logout'),
+    path('login/', views.login_user, name='login'),  # Reference your login_user function here
+    path('register_user/', views.register_user, name='register_user'),
     path('spotify/login/', spotify_login, name='spotify_login'),
     path('spotify/callback/', spotify_callback, name='spotify_callback'),
     path('spotify/user-data/', views.get_user_data, name='spotify_user_data')
+
 
 ]
 
