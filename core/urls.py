9e0ed0ad3d4f74f492f52aa41e6dfd736ee76878
Muskan Mahajan <muskanmahajan2004@gmail.com>
2024-10-23 15:29:47 -0400

    Added index, base, login, register and styles

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from .views import spotify_login, spotify_callback
 
 urlpatterns = [
-    # path('login/', views.login_user, name='login'),  # Reference your login_user function here
+    path('login/', views.login, name='login'),  # Reference your login_user function here
     # path('register_user/', views.register_user, name='register_user'),
     # path('profile/', views.profile, name='profile'),
     # path('logout/', views.logout_user, name='logout'),
